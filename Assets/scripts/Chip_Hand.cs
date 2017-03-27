c25f010a076ce9fb35c928e15e9e6411e224638f
cfinnegn <cfinnegn@udel.edu>
2017-03-27 15:43:53 -0400

    Draw Chip button functionality
    + Chip Bay empty in scene
    + Chip bay initializes with 3 chips
    + Draw Chip button adds chip to chip bay (up to 6)

---
+++ Assets/scripts/Chip_Hand.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chip_Hand : MonoBehaviour {
+	public int held;
+	public GameObject deck; // !!! for now this is a single "ChipImg" that will be cloned !!!
+	public GameObject[] chips;
+
+
+	// Use this for initialization
+	void Start () {
+		held = 3;
+		chips = new GameObject[6];
+		int i = 0;
+		Debug.Log("starting chips");
+		while(i < held) {
+			chips[i] = Instantiate(deck, transform,true);
+			chips[i].GetComponent<Transform>().localScale = new Vector3(1.0f, 1.0f, 1.0f);
+			Debug.Log(i);
+			i++;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public bool chip_added() {
+		if(held < 6) {	// not full hand
+			chips[held] = Instantiate(deck, transform,true);    // adds chip into next open position
+			chips[held].GetComponent<Transform>().localScale = new Vector3(1.0f, 1.0f, 1.0f);
+			held++;
+			return true;
+		}
+		else { return false; }
+	}
+
+	public bool chip_removed(int index) {
+		if (index < held) { // removing a chip that is in hand
+			int i = index;
+			while (i < held - 1) { //shift chips left 1 starting at index
+				chips[i] = chips[i + 1];
+				i++;
+			}
+			chips[held - 1] = null; // blanks out previous last chip of hand
+			return true;
+		}
+		return false;
+	}
+
+}
---
+++ Assets/scripts/Draw_Button.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Draw_Button : MonoBehaviour {
+	public GameObject Chip_Hand;
+	public GameObject Cust;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Draw_Chip() {
+		Chip_Hand.GetComponent<Chip_Hand>().chip_added();
+	}
+
+}
