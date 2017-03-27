bfdf1fd3f6f76666bf67c4f11ab482c994562226
cfinnegn <cfinnegn@udel.edu>
2017-03-27 16:38:28 -0400

    Randomized Chip Costs
    + Chips are created with a random persistent cost

---
+++ Assets/scripts/Cust.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cust : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/scripts/Hand_Chip.cs
+++ Assets/scripts/Hand_Chip.cs
@@ -5,16 +5,18 @@ using UnityEngine.UI;
 
 public class Hand_Chip : MonoBehaviour {
 	public int index;
+	public int cost;
 
 	// Use this for initialization
 	void Start () {
-		
+		cost = Random.Range(1, 6);
+		Transform text = transform.GetChild(0).GetChild(0);
+		text.GetComponent<Text>().text = "" + cost;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Transform text = transform.GetChild(0).GetChild(0);
-		text.GetComponent<Text>().text = ""+index;
+
 	}
 
 	public void clicked() {
