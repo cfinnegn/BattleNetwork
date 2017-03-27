110216b35085d5eebe047a124eca25afda609a99
cfinnegn <cfinnegn@udel.edu>
2017-03-27 16:34:00 -0400

    Clicking chips in hand removes them
    + Chips in hand can be removed by clicking
    + Chip indexes are reset when one is removed
    ~ Chips indexes are shown on the cost display

--- Assets/scripts/Chip_Hand.cs
+++ Assets/scripts/Chip_Hand.cs
@@ -10,15 +10,11 @@ public class Chip_Hand : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		held = 3;
+		int target_hold = 3;	// !! set starting hand size here
 		chips = new GameObject[6];
-		int i = 0;
-		Debug.Log("starting chips");
-		while(i < held) {
-			chips[i] = Instantiate(deck, transform,true);
-			chips[i].GetComponent<Transform>().localScale = new Vector3(1.0f, 1.0f, 1.0f);
-			Debug.Log(i);
-			i++;
+		held = 0;
+		while(held < target_hold) {	// loop until holding targed number
+			chip_added();
 		}
 	}
 	
@@ -31,6 +27,7 @@ public class Chip_Hand : MonoBehaviour {
 		if(held < 6) {	// not full hand
 			chips[held] = Instantiate(deck, transform,true);    // adds chip into next open position
 			chips[held].GetComponent<Transform>().localScale = new Vector3(1.0f, 1.0f, 1.0f);
+			chips[held].GetComponent<Hand_Chip>().index = held;
 			held++;
 			return true;
 		}
@@ -38,13 +35,17 @@ public class Chip_Hand : MonoBehaviour {
 	}
 
 	public bool chip_removed(int index) {
+
 		if (index < held) { // removing a chip that is in hand
+			Destroy(chips[index]);
 			int i = index;
 			while (i < held - 1) { //shift chips left 1 starting at index
 				chips[i] = chips[i + 1];
+				chips[i].GetComponent<Hand_Chip>().index = i;   // decrement index for later clicking
 				i++;
 			}
 			chips[held - 1] = null; // blanks out previous last chip of hand
+			held--;
 			return true;
 		}
 		return false;
---
+++ Assets/scripts/Hand_Chip.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hand_Chip : MonoBehaviour {
+	public int index;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Transform text = transform.GetChild(0).GetChild(0);
+		text.GetComponent<Text>().text = ""+index;
+	}
+
+	public void clicked() {
+		Debug.Log(index);
+		GameObject hand = transform.parent.gameObject;
+		hand.GetComponent<Chip_Hand>().chip_removed(index);
+	}
+}
