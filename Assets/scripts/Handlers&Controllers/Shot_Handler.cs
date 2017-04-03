c8e1307f8be471350a123749a4ad969b6a93d09c
cfinnegn <cfinnegn@udel.edu>
2017-04-03 18:15:59 -0400

    Training added to menu, Shot_Handler netcode
    + Training Mode button in menu loads battle against Dummy
    + Shot_Handler (should) dectect buster hits online
    ! online shot handler untested

--- Assets/scripts/Handlers&Controllers/Shot_Handler.cs
+++ Assets/scripts/Handlers&Controllers/Shot_Handler.cs
@@ -19,7 +19,7 @@ public class Shot_Handler : MonoBehaviour {
 	}
 
 	public bool check_bust(int dmg, int shooter) {
-		if(training) {	// no need to mirror positions in training mode
+		if(training) {  // no need to mirror positions in training mode
 			int dist = playerB.GetComponent<NPC_Navi_Rand>().field_space - playerA.GetComponent<Navi>().field_space;
 			if((0 < dist) && (dist <= 5)) {
 				if(shooter == playerA.GetComponent<Navi>().playerNumber) {
@@ -32,6 +32,27 @@ public class Shot_Handler : MonoBehaviour {
 				}
 			}
 		}
+		// mirrored space = ((space + 5) - 2*(space%6))
+		else {  // positions must be mirrored when online
+			if(shooter == playerA.GetComponent<Navi>().playerNumber) {	// PlayerA is shooter
+				// target space is space of non-shooter, mirrored as though on right side
+				int target_space = ((playerB.GetComponent<Navi>().field_space + 5) - 2 * (playerB.GetComponent<Navi>().field_space % 6));
+				int dist = target_space - playerA.GetComponent<Navi>().field_space;
+				if((0 < dist) && (dist <= 5)) {
+					playerB.GetComponent<Navi>().hit(dmg);
+					return true;
+				}
+			}
+			else { // plyaer B is shooter
+				// target space is space of non-shooter, mirrored as though on right side
+				int target_space = ((playerA.GetComponent<Navi>().field_space + 5) - 2 * (playerA.GetComponent<Navi>().field_space % 6));
+				int dist = target_space - playerA.GetComponent<Navi>().field_space;
+				if((0 < dist) && (dist <= 5)) {
+					playerB.GetComponent<Navi>().hit(dmg);
+					return true;
+				}
+			}
+	}
 		return false;
 	}
 }
